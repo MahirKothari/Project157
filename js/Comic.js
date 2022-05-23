2d2691eb1e01ac8acc264f0b12895f5aaab5e508
MahirKothari <64363014+MahirKothari@users.noreply.github.com>
2022-05-23 01:57:53 -0700

    Add files via upload

---
+++ js/Comic.js
@@ -0,0 +1,76 @@
+AFRAME.registerComponent("comic", {
+  init: function () {
+    this.placesContainer = this.el;
+    this.createCards()
+  },
+
+  createCards: function () {
+    const thumbNailsRef = [
+      {
+        id: "superman",
+        title: "SuperMan",
+        url: "./assets/thumbnails/Superman.jpg",
+      },
+      {
+        id: "spiderman",
+        title: "Spiderman",
+        url: "./assets/thumbnails/Spiderman.jpg",
+      },
+
+      {
+        id: "outer-space",
+        title: "Outer Space",
+        url: "./assets/thumbnails/OuterSpace.jpg",
+      },
+      {
+        id: "captain-aero",
+        title: "Captain Aero",
+        url: "./assets/thumbnails/CaptainAero.jpg",
+      },
+    ];
+    let prevoiusXPosition = -60;
+
+    for (var item of thumbNailsRef) {
+      const posX = prevoiusXPosition + 25;
+      const posY = 10;
+      const posZ = -40;
+      const position = { x: posX, y: posY, z: posZ };
+      prevoiusXPosition = posX;
+
+      // Border Element
+      const borderEl = this.createBorder(position,item.id)
+      // Thumbnail Element
+     const thumbnail = this.createThumbnail(item)
+     borderEl.appendChild(thumbnail)
+      // Title Text Element
+      const titleEl = this.createTitle(position,item)
+      borderEl.appendChild(titleEl)
+      this.placesContainer.appendChild(borderEl);
+    }
+  },
+  createBorder: function(position,id){
+    const entityEl = document.createElement('a-entity')
+    entityEl.setAttribute('id',id)
+    entityEl.setAttribute('visible',true)
+    entityEl.setAttribute('geometry',{primitive:'plane',width:22,height:30})
+    entityEl.setAttribute('position',position)
+    entityEl.setAttribute('material',{color:'#00bcd4',opacity:0.4})
+    return entityEl
+  },
+  createThumbnail: function(item){
+    const entityEl = document.createElement('a-entity')
+    entityEl.setAttribute('visible',true)
+    entityEl.setAttribute('geometry',{primitive:'plane',width:20,height:28})
+    entityEl.setAttribute('material',{src:item.url})
+    return entityEl
+  },
+  createTitle: function(position,item){
+    const entityEl = document.createElement('a-entity')
+    entityEl.setAttribute('visible',true)
+    entityEl.setAttribute('text',{font: 'exo2bold', align: 'center', width: 70, color: '#e65100', value: item.title})
+    const elPosition = position
+    elPosition.y = -25
+    entityEl.setAttribute('position',position)
+    return entityEl
+  }
+});
